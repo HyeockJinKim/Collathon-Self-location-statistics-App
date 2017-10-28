036d519c4e638dec07d6e60e2c5c72f7b6d37f79
GoodLuckDay <haon4658@gmail.com>
2017-10-29 00:06:55 +0900

    Self Location Marker Implement

--- app/src/main/java/comkimhyeockjin/github/selflocationstatisticsapp/MapExampleActivity.java
+++ app/src/main/java/comkimhyeockjin/github/selflocationstatisticsapp/MapExampleActivity.java
@@ -1,37 +1,56 @@
 package comkimhyeockjin.github.selflocationstatisticsapp;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Rect;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.gms.location.places.PlaceDetectionClient;
 import com.google.android.gms.location.places.PlaceLikelihood;
 import com.google.android.gms.location.places.PlaceLikelihoodBufferResponse;
 import com.google.android.gms.location.places.Places;
-import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.nhn.android.maps.NMapActivity;
+import com.nhn.android.maps.NMapCompassManager;
+import com.nhn.android.maps.NMapController;
+import com.nhn.android.maps.NMapLocationManager;
+import com.nhn.android.maps.NMapOverlay;
+import com.nhn.android.maps.NMapOverlayItem;
 import com.nhn.android.maps.NMapView;
+import com.nhn.android.maps.maplib.NGeoPoint;
+import com.nhn.android.maps.overlay.NMapPOIdata;
+import com.nhn.android.maps.overlay.NMapPOIitem;
+import com.nhn.android.mapviewer.overlay.NMapCalloutCustomOverlay;
+import com.nhn.android.mapviewer.overlay.NMapCalloutOverlay;
+import com.nhn.android.mapviewer.overlay.NMapMyLocationOverlay;
+import com.nhn.android.mapviewer.overlay.NMapOverlayManager;
+import com.nhn.android.mapviewer.overlay.NMapPOIdataOverlay;
 
 
 public class MapExampleActivity extends NMapActivity {
 
     private static final String TAG = MapExampleActivity.class.getSimpleName();
     private NMapView mMapView;
+    private NMapController mMapController;
     private final String CLIENT_ID = "Rj_aFrA9FICH0OWYVlfS";
+    private NMapOverlayManager mOverlayManager;
+    private NMapMyLocationOverlay mMyLocationOverlay;
+    private NMapLocationManager mMapLocationManager;
+    private NMapCompassManager mMapCompassManager;
+    private NMapViewerResourceProvider mMapViewerResourceProvider;
+    private MapContainerView mMapContainerView;
     private PlaceDetectionClient mPlaceDetectionClient;
-    private static final int M_MAX_ENTRIES = 5;
-
-    private String[] mLikelyPlaceNames;
-    private String[] mLikelyPlaceAddresses;
-    private String[] mLikelyPlaceAttributions;
-    private LatLng[] mLikelyPlaceLatLngs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +63,16 @@ public class MapExampleActivity extends NMapActivity {
         mMapView.setFocusable(true);
         mMapView.setFocusableInTouchMode(true);
         mMapView.requestFocus();
+
+        mMapViewerResourceProvider = new NMapViewerResourceProvider(this);
+        mOverlayManager = new NMapOverlayManager(this, mMapView, mMapViewerResourceProvider);
+        mOverlayManager.setOnCalloutOverlayListener(onCalloutOverlayListener);
+        mOverlayManager.setOnCalloutOverlayViewListener(onCalloutOverlayViewListener);
+        mMapController = mMapView.getMapController();
+        mMapLocationManager = new NMapLocationManager(this);
+        mMapLocationManager.setOnLocationChangeListener(onMyLocationChangeListener);
+        mMapCompassManager = new NMapCompassManager(this);
+        mMyLocationOverlay = mOverlayManager.createMyLocationOverlay(mMapLocationManager, mMapCompassManager);
 
         mPlaceDetectionClient = Places.getPlaceDetectionClient(this, null);
 
@@ -61,6 +90,167 @@ public class MapExampleActivity extends NMapActivity {
             }
         }.start();
 
+
+        startMyLocation();
+    }
+
+
+
+
+    private final NMapLocationManager.OnLocationChangeListener onMyLocationChangeListener = new NMapLocationManager.OnLocationChangeListener() {
+
+        @Override
+        public boolean onLocationChanged(NMapLocationManager locationManager, NGeoPoint myLocation) {
+
+            if (mMapController != null) {
+                mMapController.animateTo(myLocation);
+            }
+
+            return true;
+        }
+
+        @Override
+        public void onLocationUpdateTimeout(NMapLocationManager locationManager) {
+
+            // stop location updating
+            //			Runnable runnable = new Runnable() {
+            //				public void run() {
+            //					stopMyLocation();
+            //				}
+            //			};
+            //			runnable.run();
+
+            Toast.makeText(MapExampleActivity.this, "Your current location is temporarily unavailable.", Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onLocationUnavailableArea(NMapLocationManager locationManager, NGeoPoint myLocation) {
+
+            Toast.makeText(MapExampleActivity.this, "Your current location is unavailable area.", Toast.LENGTH_LONG).show();
+
+            stopMyLocation();
+        }
+
+    };
+
+    private final NMapOverlayManager.OnCalloutOverlayViewListener onCalloutOverlayViewListener = new NMapOverlayManager.OnCalloutOverlayViewListener() {
+
+        @Override
+        public View onCreateCalloutOverlayView(NMapOverlay itemOverlay, NMapOverlayItem overlayItem, Rect itemBounds) {
+
+
+            // null을 반환하면 말풍선 오버레이를 표시하지 않음
+            return null;
+        }
+
+    };
+
+    private final NMapOverlayManager.OnCalloutOverlayListener onCalloutOverlayListener = new NMapOverlayManager.OnCalloutOverlayListener() {
+
+        @Override
+        public NMapCalloutOverlay onCreateCalloutOverlay(NMapOverlay itemOverlay, NMapOverlayItem overlayItem,
+                                                         Rect itemBounds) {
+
+            // handle overlapped items
+            if (itemOverlay instanceof NMapPOIdataOverlay) {
+                NMapPOIdataOverlay poiDataOverlay = (NMapPOIdataOverlay)itemOverlay;
+
+                // check if it is selected by touch event
+                if (!poiDataOverlay.isFocusedBySelectItem()) {
+                    int countOfOverlappedItems = 1;
+
+                    NMapPOIdata poiData = poiDataOverlay.getPOIdata();
+                    for (int i = 0; i < poiData.count(); i++) {
+                        NMapPOIitem poiItem = poiData.getPOIitem(i);
+
+                        // skip selected item
+                        if (poiItem == overlayItem) {
+                            continue;
+                        }
+
+                        // check if overlapped or not
+                        if (Rect.intersects(poiItem.getBoundsInScreen(), overlayItem.getBoundsInScreen())) {
+                            countOfOverlappedItems++;
+                        }
+                    }
+
+                    if (countOfOverlappedItems > 1) {
+                        String text = countOfOverlappedItems + " overlapped items for " + overlayItem.getTitle();
+                        Toast.makeText(MapExampleActivity.this, text, Toast.LENGTH_LONG).show();
+                        return null;
+                    }
+                }
+            }
+
+            // use custom old callout overlay
+            if (overlayItem instanceof NMapPOIitem) {
+                NMapPOIitem poiItem = (NMapPOIitem)overlayItem;
+
+                if (poiItem.showRightButton()) {
+                    return new NMapCalloutCustomOldOverlay(itemOverlay, overlayItem, itemBounds,
+                            mMapViewerResourceProvider);
+                }
+            }
+
+            // use custom callout overlay
+            return new NMapCalloutCustomOverlay(itemOverlay, overlayItem, itemBounds, mMapViewerResourceProvider);
+
+            // set basic callout overlay
+            //return new NMapCalloutBasicOverlay(itemOverlay, overlayItem, itemBounds);
+        }
+
+    };
+    private void startMyLocation() {
+
+        if (mMyLocationOverlay != null) {
+            if (!mOverlayManager.hasOverlay(mMyLocationOverlay)) {
+                mOverlayManager.addOverlay(mMyLocationOverlay);
+            }
+
+            if (mMapLocationManager.isMyLocationEnabled()) {
+
+                if (!mMapView.isAutoRotateEnabled()) {
+                    mMyLocationOverlay.setCompassHeadingVisible(true);
+
+                    mMapCompassManager.enableCompass();
+
+                    mMapView.setAutoRotateEnabled(true, false);
+
+                    mMapContainerView.requestLayout();
+                } else {
+                    stopMyLocation();
+                }
+
+                mMapView.postInvalidate();
+            } else {
+                boolean isMyLocationEnabled = mMapLocationManager.enableMyLocation(true);
+                if (!isMyLocationEnabled) {
+                    Toast.makeText(MapExampleActivity.this, "Please enable a My Location source in system settings",
+                            Toast.LENGTH_LONG).show();
+
+                    Intent goToSettings = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    startActivity(goToSettings);
+
+                    return;
+                }
+            }
+        }
+    }
+
+    private void stopMyLocation() {
+        if (mMyLocationOverlay != null) {
+            mMapLocationManager.disableMyLocation();
+
+            if (mMapView.isAutoRotateEnabled()) {
+                mMyLocationOverlay.setCompassHeadingVisible(false);
+
+                mMapCompassManager.disableCompass();
+
+                mMapView.setAutoRotateEnabled(false, false);
+
+                mMapContainerView.requestLayout();
+            }
+        }
     }
 
 
@@ -95,6 +285,34 @@ public class MapExampleActivity extends NMapActivity {
             }
         });
     }
+
+
+    private class MapContainerView extends ViewGroup {
+
+        public MapContainerView(Context context) {
+            super(context);
+        }
+
+        @Override
+        protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+            final int width = getWidth();
+            final int height = getHeight();
+            final int count = getChildCount();
+            for (int i = 0; i < count; i++) {
+                final View view = getChildAt(i);
+                final int childWidth = view.getMeasuredWidth();
+                final int childHeight = view.getMeasuredHeight();
+                final int childLeft = (width - childWidth) / 2;
+                final int childTop = (height - childHeight) / 2;
+                view.layout(childLeft, childTop, childLeft + childWidth, childTop + childHeight);
+            }
+
+            if (changed) {
+                mOverlayManager.onSizeChanged(width, height);
+            }
+        }
+    }
+
 
 
 }
