d4c5951a31b7950cb0c04c6582799cf32c59f354
KSJ <abbey17@naver.com>
2017-10-30 14:23:17 +0900

    그래프액티비티 UI 디자인 및 UserActivity와 연결

---
+++ app/src/main/java/comkimhyeockjin/github/selflocationstatisticsapp/GraphActivity.java
@@ -0,0 +1,19 @@
+package comkimhyeockjin.github.selflocationstatisticsapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by SUDALKIM on 2017-10-30.
+ */
+
+public class GraphActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_graph);
+
+
+    }
+
+}
--- app/src/main/java/comkimhyeockjin/github/selflocationstatisticsapp/UserActivity.java
+++ app/src/main/java/comkimhyeockjin/github/selflocationstatisticsapp/UserActivity.java
@@ -3,6 +3,8 @@ package comkimhyeockjin.github.selflocationstatisticsapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by SUDALKIM on 2017-10-23.
@@ -17,6 +19,15 @@ public class UserActivity extends Activity{
 
         Intent intent = new Intent(this, LoginActivity.class);
         startActivityForResult(intent, USER_REQUEST);
+
+        Button graphBtn = (Button) findViewById(R.id.graphBtn);
+        graphBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent temp_intent = new Intent(getApplicationContext(), GraphActivity.class);
+                startActivity(temp_intent);
+            }
+        });
 
     }
 
