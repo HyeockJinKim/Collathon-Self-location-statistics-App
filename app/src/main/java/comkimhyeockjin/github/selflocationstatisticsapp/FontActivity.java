c68f609d0ca25b1cb3d98ed35a4669fec442e04d
KSJ <abbey17@naver.com>
2017-11-04 23:12:42 +0900

    font 폴더 삭제 후 assets 폴더에 다시 font 폴더 추가, 빌드 싱크 맞춤

---
+++ app/src/main/java/comkimhyeockjin/github/selflocationstatisticsapp/FontActivity.java
@@ -0,0 +1,16 @@
+package comkimhyeockjin.github.selflocationstatisticsapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by SUDALKIM on 2017-11-04.
+ */
+
+public class FontActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+
+}
