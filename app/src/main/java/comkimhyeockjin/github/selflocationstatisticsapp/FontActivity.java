655ac453e221cf149d933881c9b1f16c41063068
KSJ <abbey17@naver.com>
2017-11-04 23:17:57 +0900

    FontActivity 생성 후 Application 상속, 글꼴 설정

--- app/src/main/java/comkimhyeockjin/github/selflocationstatisticsapp/FontActivity.java
+++ app/src/main/java/comkimhyeockjin/github/selflocationstatisticsapp/FontActivity.java
@@ -1,16 +1,22 @@
 package comkimhyeockjin.github.selflocationstatisticsapp;
 
-import android.app.Activity;
-import android.os.Bundle;
+import android.app.Application;
+import android.icu.text.DisplayContext;
+
+import com.tsengvn.typekit.Typekit;
+
 
 /**
  * Created by SUDALKIM on 2017-11-04.
  */
 
-public class FontActivity extends Activity {
+public class FontActivity extends Application {
     @Override
-    protected void onCreate(Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
+    public void onCreate(){
+        super.onCreate();
+
+        Typekit.getInstance().addNormal(Typekit.createFromAsset(this, "font/NanumBarunGothic.otf")).addBold(Typekit.createFromAsset(this, "font/NanumBarunGothicBold.otf"));
+
     }
 
 }
